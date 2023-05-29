001a73dae3807b71064aa482d9300b982aaf6338
Jesus Alfonso <jesus.alfonso@ecommfarm.com>
2023-05-29 11:35:42 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return Notifications::send(
         'success',
-        'Data has been stored.'
+        'Data has been stored.',
+        false
     )->view('welcome');
 });
